7096a62dd5cea1163e8cc684ca4c5d54f69b4d78
Roger Luethi <rl@patchworkscience.org>
2017-11-14 09:18:34 +0100

    Add symlinks for tacker-horizon
    Help apache find tacker-horizon's virtual env.

--- recipes/tacker-horizon.rb
+++ recipes/tacker-horizon.rb
@@ -18,4 +18,25 @@ python_package 'tacker-horizon' do
   version tacker_horizon_version
   notifies :run, 'execute[openstack-dashboard collectstatic]'
 end
+
+link '/usr/local/lib/python2.7/dist-packages/tacker_horizon' do
+  to "#{pyenv_dir}/lib/python2.7/site-packages/tacker_horizon"
+end
+
+distinfo_dir = "tacker_horizon-#{tacker_horizon_version}.dist-info"
+
+link "/usr/local/lib/python2.7/dist-packages/#{distinfo_dir}" do
+  to "#{pyenv_dir}/lib/python2.7/site-packages/#{distinfo_dir}"
+end
+
+link '/usr/share/openstack-dashboard/openstack_dashboard/enabled/_80_nfv.py' do
+  to "#{pyenv_dir}/lib/python2.7/site-packages/tacker_horizon/enabled/_80_nfv.py"
+end
+
+file '/usr/local/lib/python2.7/dist-packages/tacker_horizon/__init__.py' do
+  owner 'root'
+  group 'root'
+  mode '0644'
+  content ''
+end
 #------------------------------------------------------------------------------
